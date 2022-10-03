a8730f8db1520670075c5e01d80a1a719e9f141f
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-10-03 08:48:08 +0200

    refactor: quote endpoints

--- src/routers/quote.js
+++ src/routers/quote.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const Quote = require("../models/quote");
+const auth = require("../middleware/auth");
 
 const router = new Router();
 
-router.post("/quotes", async (req, res) => {
-  const quote = new Quote(req.body);
+router.post("/quotes", auth, async (req, res) => {
+  const quote = new Quote({
+    ...req.body,
+    owner: req.user._id,
+  });
   try {
     await quote.save();
     res.status(201).send(quote);
@@ -13,18 +17,21 @@ router.post("/quotes", async (req, res) => {
   }
 });
 
-router.get("/quotes", async (req, res) => {
+router.get("/quotes", auth, async (req, res) => {
   try {
-    const quotes = await Quote.find({});
+    const quotes = await Quote.find({ owner: req.user._id });
     res.send(quotes);
   } catch (e) {
     res.status(500).send();
   }
 });
 
-router.delete("/quotes/:id", async (req, res) => {
+router.delete("/quotes/:id", auth, async (req, res) => {
   try {
-    const quote = await Quote.findByIdAndDelete(req.params.id);
+    const quote = await Quote.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
     if (!quote) {
       return res.status(404).send();
     }
