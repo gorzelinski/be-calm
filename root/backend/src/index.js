01d37c0abc5487de346f1dcd75c14b120d48565b
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-10-06 12:36:32 +0200

    feat: add cors middleware

--- root/backend/src/index.js
+++ root/backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require("cors");
 const userRouter = require("./routers/user");
 const quoteRouter = require("./routers/quote");
 
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(quoteRouter);
 
